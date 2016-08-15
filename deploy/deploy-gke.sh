c31653baf277804d427c3e3a3cae2f1d26d2b728
Adriell Matthew Julius Dagasuan <adrielldagasuan@gmail.com>
2016-08-15 12:29:27 +0800

    test

--- deploy/deploy-gke.sh
+++ deploy/deploy-gke.sh
@@ -5,4 +5,5 @@ set -e
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 docker push adrielldagasuan/products-service-gke:$CIRCLE_SHA1
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"docker.io/adrielldagasuan/products-service-gke:'"$CIRCLE_SHA1"'"}]}}}}'
