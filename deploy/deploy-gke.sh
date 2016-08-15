7c8dee4fd4c59de4e5af3fc41c96aed706d23976
Adriell Matthew Julius Dagasuan <adrielldagasuan@gmail.com>
2016-08-15 12:24:05 +0800

    fix deploy.sh

--- deploy/deploy-gke.sh
+++ deploy/deploy-gke.sh
@@ -3,6 +3,6 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push docker.io/adrielldagasuan/products-service-gke
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker push adrielldagasuan/products-service-gke:$CIRCLE_SHA1
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"docker.io/adrielldagasuan/products-service-gke:'"$CIRCLE_SHA1"'"}]}}}}'
