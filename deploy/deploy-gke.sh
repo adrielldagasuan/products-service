d2146b495efe52029ad823175e3135d224ffdc91
unknown <dagasuan@DAGASUAN3.asiapacific.hpqcorp.net>
2016-08-15 16:40:22 +0800

    add back into deploy script

--- deploy/deploy-gke.sh
+++ deploy/deploy-gke.sh
@@ -4,6 +4,7 @@
 set -e
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker push adrielldagasuan/products-service-gke:$CIRCLE_SHA1
+docker push adrielldagasuan/products-service:$CIRCLE_SHA1
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"docker.io/adrielldagasuan/products-service-gke:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment products-service -p '{"spec":{"template":{"spec":{"containers":[{"name":"products-service","image":"docker.io/adrielldagasuan/products-service:'"$CIRCLE_SHA1"'"}]}}}}'
